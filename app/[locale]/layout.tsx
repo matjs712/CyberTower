b1206c3116324d413d4bf5cf75d7dfd9e0c9040e
jsmat777 <matjs712@gmail.com>
2025-11-09 00:24:48 -0300

    change ddefault theme

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -50,7 +50,7 @@ export default async function RootLayout({
         <NextIntlClientProvider locale={locale} messages={messages}>
           <ThemeProvider
             attribute="class"
-            defaultTheme="dark"
+            // defaultTheme="dark"
             enableSystem={false}
             disableTransitionOnChange
           >
