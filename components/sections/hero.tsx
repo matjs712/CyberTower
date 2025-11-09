00798a9a10b469684ea16d51cfad5801b48e9197
jsmat777 <matjs712@gmail.com>
2025-11-09 00:06:04 -0300

    a

--- components/sections/hero.tsx
+++ components/sections/hero.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { motion } from "framer-motion";
-import Image from "next/image";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import { Mail } from "lucide-react";
