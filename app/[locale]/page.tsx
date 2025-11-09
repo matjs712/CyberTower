9e18b323c2cb508b0e428a891a10888d5308eb94
jsmat777 <matjs712@gmail.com>
2025-11-08 23:30:41 -0300

    adada

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -3,7 +3,6 @@ import ClientesSection from "@/components/sections/clientes/clientes";
 import Hero from "@/components/sections/hero";
 import Highlights from "@/components/sections/highlights";
 import Mision from "@/components/sections/mision";
-// import Opinion from "@/components/sections/opinion";
 import CoreServices from "@/components/sections/services";
 import { setRequestLocale } from "next-intl/server";
 
